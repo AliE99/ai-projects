faff468151860baf1141279539ddc677e58f6b9b
Ali Elahi <alielahi.1378@gmail.com>
2022-01-25 15:37:01 +0330

    Feat: Refactor GA code

--- genetic_algorithm/ga.py
+++ genetic_algorithm/ga.py
@@ -8,34 +8,34 @@ import matplotlib.pyplot as plt
 # Initializing the list of items, we have 10 items
 item_number = np.arange(1, 11)
 weight = np.random.randint(1, 15, size=10)
-value = np.random.randint(10, 750, size=10)
-knapsack_threshold = 35  # Maximum weight of the bag
-# print('The list is as follows:')
-# print('Item No.   Weight   Value')
-# for i in range(item_number.shape[0]):
-#     print(f"{item_number[i]}          {weight[i]}         {value[i]}\n")
+cost = np.random.randint(10, 750, size=10)
+knapsack_limit = 35  # Maximum weight of the bag
+print('The Items:')
+print('Number   Weight   Cost')
+for i in range(item_number.shape[0]):
+    print(f"{item_number[i]}          {weight[i]}         {cost[i]}\n")
 
 
 # Initial Population
-# Each gene has a value 1 or 0 which tells whether the corresponding item is present or not
-solutions_per_pop = 8
-pop_size = (solutions_per_pop, item_number.shape[0])
-print('Population size = {}'.format(pop_size))
-initial_population = np.random.randint(2, size=pop_size)
+# Each gene has a cost 1 or 0 which tells whether the corresponding item is present or not
+solutionsPopulation = 8
+populationSize = (solutionsPopulation, item_number.shape[0])
+print(f"Population size = {populationSize}")
+initial_population = np.random.randint(2, size=populationSize)
 initial_population = initial_population.astype(int)
-num_generations = 50
-print('Initial population: \n{}'.format(initial_population))
+numberOfGenerations = 50
+print(f"Initial population: \n{initial_population}")
 
 
-# The fitness function
-def cal_fitness(weight, value, population, threshold):
+# The fitness function, limit = knapsack weight
+def fitnessFunction(weight, cost, population, limit):
     fitness = np.empty(population.shape[0])
     for i in range(population.shape[0]):
-        # Calculate value of chromosome
-        S1 = np.sum(population[i] * value)
-        S2 = np.sum(population[i] * weight)
-        if S2 <= threshold:
-            fitness[i] = S1
+        # Calculate cost of chromosome
+        sigma_1 = np.sum(population[i] * cost)
+        sigma_2 = np.sum(population[i] * weight)
+        if sigma_2 <= limit:
+            fitness[i] = sigma_1
         else:
             fitness[i] = 0
     return fitness.astype(int)
@@ -61,13 +61,13 @@ def crossover(parents, num_offsprings):
     while (parents.shape[0] < num_offsprings):
         parent1_index = i % parents.shape[0]
         parent2_index = (i+1) % parents.shape[0]
-        x = rd.random()
-        if x > crossover_rate:
+        crossover_chance = rd.random()
+        if crossover_chance > crossover_rate:
             continue
         parent1_index = i % parents.shape[0]
         parent2_index = (i+1) % parents.shape[0]
         offsprings[i, 0:crossover_point] = parents[parent1_index,
-                                                   0:crossover_point]
+                                                   0: crossover_point]
         offsprings[i, crossover_point:] = parents[parent2_index,
                                                   crossover_point:]
         i = +1
@@ -94,32 +94,32 @@ def mutation(offsprings):
 
 # now we will call them in the order of the flow chart to find the required parameters
 # and make all the necessary initializations.
-def optimize(weight, value, population, pop_size, num_generations, threshold):
-    parameters, fitness_history = [], []
-    num_parents = int(pop_size[0]/2)
-    num_offsprings = pop_size[0] - num_parents
-    for i in range(num_generations):
-        fitness = cal_fitness(weight, value, population, threshold)
-        fitness_history.append(fitness)
+def optimization(weight, cost, population, populationSize, numberOfGenerations, limit):
+    parameters = []
+    num_parents = int(populationSize[0]/2)
+    num_offsprings = populationSize[0] - num_parents
+    for i in range(numberOfGenerations):
+        fitness = fitnessFunction(weight, cost, population, limit)
         parents = selection(fitness, num_parents, population)
         offsprings = crossover(parents, num_offsprings)
         mutants = mutation(offsprings)
         population[0:parents.shape[0], :] = parents
         population[parents.shape[0]:, :] = mutants
 
-    print('Last generation: \n{}\n'.format(population))
-    fitness_last_gen = cal_fitness(weight, value, population, threshold)
-    print('Fitness of the last generation: \n{}\n'.format(fitness_last_gen))
+    print('Last generation: \n{population}\n')
+    fitness_last_gen = fitnessFunction(weight, cost, population, limit)
+    print("Fitness of the last generation: \n{fitness_last_gen}\n")
     max_fitness = np.where(fitness_last_gen == np.max(fitness_last_gen))
     parameters.append(population[max_fitness[0][0], :])
-    return parameters, fitness_history
+    return parameters
 
 
-parameters, fitness_history = optimize(
-    weight, value, initial_population, pop_size, num_generations, knapsack_threshold)
-print('The optimized parameters for the given inputs are: \n{}'.format(parameters))
+parameters = optimization(
+    weight, cost, initial_population, populationSize, numberOfGenerations, knapsack_limit)
+
+print(f"The optimized parameters: \n{parameters}")
 selected_items = item_number * parameters
-print('\nSelected items that will maximize the knapsack without breaking it:')
+print("\nSelected items for this problem: ")
 for i in range(selected_items.shape[1]):
     if selected_items[0][i] != 0:
-        print('{}'.format(selected_items[0][i]))
+        print(selected_items[0][i])
